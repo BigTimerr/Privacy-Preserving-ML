abc30da9ce6a6a913c86b67293340fd76e2da8ab
ruetrash <bigtimerr@163.com>
2023-03-15 11:07:28 +0800

    增加device属性，但是在cuda上计算矩阵点乘仍有问题

--- ProtocolOnRing/secret_sharing_fixpoint.py
+++ ProtocolOnRing/secret_sharing_fixpoint.py
@@ -30,14 +30,18 @@ ptr = 0
 
 
 class ShareFloat(object):
-    def __init__(self, value, p, tcp):
+    def __init__(self, value, p, tcp, device):
         self.value = value
         self.p = p
         self.tcp = tcp
+        self.device = device
         if p == 0:
             self.party = 'server'
         else:
             self.party = 'client'
+
+        if self.device == "cuda":
+            self.value = self.value.to(device)
 
     def __str__(self):
         return "[{} value:{},\n party:{}]".format(self.__class__.__name__, self.value, self.party)
@@ -47,33 +51,33 @@ class ShareFloat(object):
             return sec_add(self, other)
         elif isinstance(other, int):
             if self.p == 0:
-                y_share = ShareFloat(value=other, p=self.p, tcp=self.tcp)
+                y_share = ShareFloat(value=other, p=self.p, tcp=self.tcp, device=self.device)
             else:
-                y_share = ShareFloat(value=0, p=self.p, tcp=self.tcp)
+                y_share = ShareFloat(value=0, p=self.p, tcp=self.tcp, device=self.device)
             return sec_add(self, y_share)
 
     def __neg__(self):
         neg_value = (-self.value) % Q
-        return ShareFloat(value=neg_value, p=self.p, tcp=self.tcp)
+        return ShareFloat(value=neg_value, p=self.p, tcp=self.tcp, device=self.device)
 
     def __sub__(self, other):
         if isinstance(other, ShareFloat):
             return sec_add(self, -other)
         elif isinstance(other, int):
             if self.p == 0:
-                y_share = ShareFloat(value=-other, p=self.p, tcp=self.tcp)
+                y_share = ShareFloat(value=-other, p=self.p, tcp=self.tcp, device=self.device)
             else:
-                y_share = ShareFloat(value=0, p=self.p, tcp=self.tcp)
+                y_share = ShareFloat(value=0, p=self.p, tcp=self.tcp, device=self.device)
             return sec_add(self, y_share)
 
     def __mul__(self, other):
         if isinstance(other, ShareFloat):
             return sec_mul_float(self, other)
         elif isinstance(other, int):
-            res = ShareFloat(value=(self.value * other), p=self.p, tcp=self.tcp)
+            res = ShareFloat(value=(self.value * other), p=self.p, tcp=self.tcp, device=self.device)
             return res
         else:
-            res = ShareFloat(value=(self.value * other), p=self.p, tcp=self.tcp)
+            res = ShareFloat(value=(self.value * other), p=self.p, tcp=self.tcp, device=self.device)
             return res
 
     def __gt__(self, other):
@@ -82,7 +86,7 @@ class ShareFloat(object):
             w = -le.value + 1
         else:
             w = -le.value
-        return ShareFloat(w, self.p, self.tcp)
+        return ShareFloat(w, self.p, self.tcp, device=self.device)
 
     def __lt__(self, other):
         ge = sec_great_eq_MSB(self, other)
@@ -90,7 +94,7 @@ class ShareFloat(object):
             w = -ge.value + 1
         else:
             w = -ge.value
-        return ShareFloat(value=w, p=self.p, tcp=self.tcp)
+        return ShareFloat(value=w, p=self.p, tcp=self.tcp, device=self.device)
 
     def __ge__(self, other):
         if isinstance(other, ShareFloat):
@@ -101,7 +105,7 @@ class ShareFloat(object):
             return sec_less_eq_MSB(self, other)
 
     def __getitem__(self, item):
-        r = ShareFloat(value=self.value[item], p=self.p, tcp=self.tcp)
+        r = ShareFloat(value=self.value[item], p=self.p, tcp=self.tcp, device=self.device)
         return r
 
     def __setitem__(self, key, value):
@@ -126,8 +130,7 @@ def decode(t):
 
 
 def share_float(t):
-    t_0 = torch.randint(0, int(Ring / 2), t.shape, dtype=torch.int64)
-    # t_0 = torch.randint(0, Q, t.shape)
+    t_0 = torch.randint(0, int(Ring / 2), t.shape, dtype=torch.int64, device=t.device)
     t_1 = (t - t_0) % Q
 
     return t_0, t_1
@@ -135,7 +138,8 @@ def share_float(t):
 
 def restore_float(t):
     t.tcp.send_torch_array(t.value)
-    other_share = t.tcp.receive_torch_array()
+    other_share = t.tcp.receive_torch_array(t.device)
+    other_share = other_share.to(t.device)
     res = (other_share + t.value) % Q
 
     return res
@@ -143,7 +147,7 @@ def restore_float(t):
 
 def restore_tensor(t):
     t.tcp.send_torch_array(t.value)
-    other_share = t.tcp.receive_torch_array()
+    other_share = t.tcp.receive_torch_array(t.device)
     res = (other_share + t.value) % Q
     res = torch.where(res > Q / 2, res - Q, res)
 
@@ -151,20 +155,20 @@ def restore_tensor(t):
 
 
 def int2bite_arr(z, size):
-    arr = torch.zeros(size=(size, 32)).bool()
+    arr = torch.zeros(size=(size, 32), device=z.device).bool()
     for i in range(0, 32):
         arr[:, i] = ((z.value >> i) & 0x01).reshape(1, size)
 
     return arr
 
 
-def img2col(img, k_size, stride=1):
+def img2col(img, k_size, stride, device):
     N, C, H, W = img.shape[0], img.shape[1], img.shape[2], img.shape[3]
     out_h = (H - k_size) // stride + 1
     out_w = (W - k_size) // stride + 1
     kw = kh = k_size
     out_size = out_w * out_h
-    col = torch.zeros(size=(N, C, kw * kh, out_size), dtype=torch.int64)
+    col = torch.zeros(size=(N, C, kw * kh, out_size), dtype=torch.int64, device=device)
     for y in range(out_h):
         y_start = y * stride
         y_end = y_start + kh
@@ -179,30 +183,32 @@ def truncate(res):
     if res.p == 0:
         # apply mask known only by P0, and reconstruct masked b to P1 or P2
         mask = (torch.ones(res.value.shape, dtype=torch.int64) * random.randrange(2**31)).long()
+        mask = mask.to(res.device)
         mask_low = (mask % scaled)
 
         res.tcp.send_torch_array((res.value + mask) % Q)
-        b_masked_low = res.tcp.receive_torch_array()
+        b_masked_low = res.tcp.receive_torch_array(res.device)
 
         b_low = (b_masked_low - mask_low) % Q
         res.value = (res.value - b_low) % Q
         z = (res.value * INVERSE) % Q
 
-        return ShareFloat(z, res.p, res.tcp)
+        return ShareFloat(z, res.p, res.tcp, res.device)
 
     if res.p == 1:
-        mask_low = res.tcp.receive_torch_array()
+        mask_low = res.tcp.receive_torch_array(res.device)
+
         b_masked = (mask_low + res.value) % Q
         b_masked_low = b_masked % scaled
         res.tcp.send_torch_array(b_masked_low)
 
         z = (res.value * INVERSE) % Q
 
-        return ShareFloat(z, res.p, res.tcp)
+        return ShareFloat(z, res.p, res.tcp, res.device)
 
 
 def sec_add(x, y) -> ShareFloat:
-    return ShareFloat(value=(x.value + y.value) % Q, p=x.p, tcp=x.tcp)
+    return ShareFloat(value=(x.value + y.value) % Q, p=x.p, tcp=x.tcp, device=x.device)
 
 
 def sec_mul_float(x, y):
@@ -211,21 +217,24 @@ def sec_mul_float(x, y):
 
     (a, b, c) = tf.get_triples(p, ptr)
 
-    a = ShareFloat(value=a, p=p, tcp=x.tcp)
-    b = ShareFloat(value=b, p=p, tcp=x.tcp)
-    c = ShareFloat(value=c, p=p, tcp=x.tcp)
+    a = torch.ones(x.value.shape, dtype=torch.int64, device=x.device) * a
+    b = torch.ones(x.value.shape, dtype=torch.int64, device=x.device) * b
+    c = torch.ones(x.value.shape, dtype=torch.int64, device=x.device) * c
 
-    e_h = ShareFloat(value=(x.value - a.value) % Q, p=p, tcp=x.tcp)
-    f_h = ShareFloat(value=(y.value - b.value) % Q, p=p, tcp=x.tcp)
+    a = ShareFloat(value=a, p=p, tcp=x.tcp, device=x.device)
+    b = ShareFloat(value=b, p=p, tcp=x.tcp, device=x.device)
+    c = ShareFloat(value=c, p=p, tcp=x.tcp, device=x.device)
+
+    e_h = ShareFloat(value=(x.value - a.value) % Q, p=p, tcp=x.tcp, device=x.device)
+    f_h = ShareFloat(value=(y.value - b.value) % Q, p=p, tcp=x.tcp, device=x.device)
 
     e = restore_float(e_h)
     f = restore_float(f_h)
 
     res = (p * e * f + e * b.value + a.value * f + c.value) % Q
-    res = ShareFloat(res, p, x.tcp)
+    res = ShareFloat(res, p, x.tcp, x.device)
 
     res = truncate(res)
-    # res.value = res.value * BASE ** (-1 * LEN_DECIMAL)
 
     return res
 
@@ -235,56 +244,68 @@ def sec_mat_mul(x: ShareFloat, y: ShareFloat):
     p = x.p
 
     (a, b, c) = tf.get_triples(p, ptr)
-    tem_value = x.value @ y.value
+    # tem_value = x.value @ y.value
+    tem_value = torch.ones(x.value.shape) @ torch.ones(y.value.shape)
     m_0 = x.value.shape[-1]
     c = (c * m_0) % Q
 
-    a_v = torch.ones(x.value.shape, dtype=x.value.dtype) * a
-    b_v = torch.ones(y.value.shape, dtype=x.value.dtype) * b
-    c_v = torch.ones(tem_value.shape, dtype=x.value.dtype) * c
+    a_v = torch.ones(x.value.shape, dtype=x.value.dtype, device=x.device) * a
+    b_v = torch.ones(y.value.shape, dtype=x.value.dtype, device=x.device) * b
+    c_v = torch.ones(tem_value.shape, dtype=x.value.dtype, device=x.device) * c
 
-    e_h = ShareFloat(value=x.value - a, p=p, tcp=x.tcp)
-    f_h = ShareFloat(value=y.value - b, p=p, tcp=x.tcp)
+    e_h = ShareFloat(value=x.value - a_v, p=p, tcp=x.tcp, device=x.device)
+    f_h = ShareFloat(value=y.value - b_v, p=p, tcp=x.tcp, device=x.device)
 
     e = restore_float(e_h)
     f = restore_float(f_h)
+
+    if x.device == "cuda":
+        e = e.to(torch.float64)
+        f = f.to(torch.float64)
+        a_v = a_v.to(torch.float64)
+        b_v = b_v.to(torch.float64)
 
     res1 = torch.matmul(e, f) % Q
     res2 = torch.matmul(a_v, f) % Q
     res3 = torch.matmul(e, b_v) % Q
 
+    if x.device == "cuda":
+
+        res1 = res1.long()
+        res2 = res2.long()
+        res3 = res3.long()
+
     res = (x.p * res1 + res2 + res3 + c_v) % Q
-    res = ShareFloat(value=res, p=x.p, tcp=x.tcp)
+    res = ShareFloat(value=res, p=x.p, tcp=x.tcp, device=x.device)
 
     res = truncate(res)
-    # print(decode(restore_float(res)))
     return res
 
 
-def B2A(arr, p, tcp):
+def B2A(arr, p, tcp, device):
     k, h = shape = arr.shape
 
-    a0_para = torch.zeros((k, h), dtype=torch.int32)
-    a1_para = torch.ones((k, h), dtype=torch.int32)
-    b0_para = torch.zeros((k, h), dtype=torch.int32)
-    b1_para = torch.ones((k, h), dtype=torch.int32)
-    c0_para = torch.zeros((k, h), dtype=torch.int32)
-    c1_para = torch.ones((k, h), dtype=torch.int32)
+    a0_para = torch.zeros((k, h), dtype=torch.int32, device=device)
+    a1_para = torch.ones((k, h), dtype=torch.int32, device=device)
+    b0_para = torch.zeros((k, h), dtype=torch.int32, device=device)
+    b1_para = torch.ones((k, h), dtype=torch.int32, device=device)
+    c0_para = torch.zeros((k, h), dtype=torch.int32, device=device)
+    c1_para = torch.ones((k, h), dtype=torch.int32, device=device)
 
     if p == 1:
         arr_a = arr.long()
-        arr_b = torch.zeros(shape, dtype=torch.int32)
+        arr_b = torch.zeros(shape, dtype=torch.int32, device=device)
         e_i = arr_a - a0_para
         f_i = arr_b - b0_para
     else:
-        arr_a = torch.zeros(shape, dtype=torch.int32)
+        arr_a = torch.zeros(shape, dtype=torch.int32, device=device)
         arr_b = arr.long()
         e_i = arr_a - a1_para
         f_i = arr_b - b1_para
 
     # get a*b
-    e = restore_tensor(ShareFloat(e_i, p, tcp))
-    f = restore_tensor(ShareFloat(f_i, p, tcp))
+    e = restore_tensor(ShareFloat(e_i, p, tcp, device))
+    f = restore_tensor(ShareFloat(f_i, p, tcp, device))
 
     if p == 1:
         ab_i = ((e * b0_para) + (f * a0_para) + c0_para) % Q
@@ -309,12 +330,12 @@ def sec_great_eq_MSB(x: ShareFloat, y: ShareFloat) -> ShareFloat:
     z.value = z.value.reshape(1, size)
     z.value = int2bite_arr(z, size)
 
-    cb = utils_vector.get_carry_bit_sonic(z, size)
-    cb = cb.reshape(1, cb.numel())
+    carry_bit = utils_vector.get_carry_bit_sonic(z, size, z.device)
+    carry_bit = carry_bit.reshape(1, carry_bit.numel())
 
-    res = B2A(cb, x.p, x.tcp)
+    res = B2A(carry_bit, x.p, x.tcp, x.device)
     res = res.reshape(x.value.shape)
-    res = ShareFloat(res, x.p, x.tcp)
+    res = ShareFloat(res, x.p, x.tcp, x.device)
 
     return res
 
@@ -323,6 +344,13 @@ def debug(x):
     if isinstance(x, ShareFloat):
         res = decode(restore_float(x))
         print(res)
+        return res
+
+def debug_no_print(x):
+    if isinstance(x, ShareFloat):
+        res = decode(restore_float(x))
+        # print(res)
+        return res
 
 
 def debug_restore(x):
