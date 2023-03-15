b70d6bc5c8a6f3e355cb26856d995356c8432e21
ruetrash <bigtimerr@163.com>
2023-03-15 11:55:54 +0800

    model 和 layer 文件更新

--- ModelAndLayers/layers/layers_of_fixpoints.py
+++ ModelAndLayers/layers/layers_of_fixpoints.py
@@ -10,29 +10,35 @@ from ProtocolOnRing.secret_sharing_fixpoint import ShareFloat
 import ProtocolOnRing.param as param
 import torch.nn.functional as F
 
-Q = param.Q
+Q = param.Ring
 BASE = param.BASE
 scaled = param.scaled
 
 
 class Layer(object):
-    def __init__(self, name):
+    def __init__(self, input_name=None, output_name=None, name=None):
+        self.input_name = input_name
+        self.output_name = output_name
         self.name = name
 
     def get_name(self):
         return self.name
 
+    def set_input_and_output(self, input_name, output_name):
+        self.input_name = input_name
+        self.output_name = output_name
+
 
 class SecConv2d(Layer):
-    def __init__(self, weight, stride=(1, 1), padding=(0, 0), bias=None, name="Conv2D"):
+    def __init__(self, weight, stride=(1, 1), padding=(0, 0), bias=None, device="cpu", name="Conv2D"):
         super(SecConv2d, self).__init__(name=name)
         self.weight = weight
         self.kernel_shape = None
-        if type(stride) is tuple:
+        if type(stride) in (tuple, list):
             self.stride = stride[0]
         else:
             self.stride = stride
-        if type(padding) is tuple:
+        if type(padding) in (tuple, list):
             padding = padding[0]
         else:
             padding = padding
@@ -41,6 +47,9 @@ class SecConv2d(Layer):
         self.input = None
         self.out_shape = None
         self.bias = bias
+        self.device = device
+        if self.device == "cuda":
+            self.weight = self.weight.to(device)
 
     def get_out_shape(self, x: ShareFloat):
         n, img_c, img_h, img_w = x.value.shape
@@ -62,11 +71,12 @@ class SecConv2d(Layer):
         padding = self.padding
         x.value = F.pad(x.value, (padding, padding, padding, padding), mode="constant", value=0)
 
-        weight = ShareFloat(self.weight, p, tcp)
+        weight_ = ShareFloat(self.weight, p, tcp, self.device)
+        weight = ShareFloat(weight_.value, p, tcp, self.device)
         kN, kC, ksize, _ = self.kernel_shape
 
         # 首先处理图片和卷积核的形状问题 ->>>
-        x.value = ssf.img2col(x.value, ksize, self.stride).transpose(1, 2)
+        x.value = ssf.img2col(x.value, ksize, self.stride, self.device).transpose(1, 2)
         weight.value = weight.value.reshape((kN, kC * ksize * ksize)).T
 
         # 将图片和卷积核送入到函数中进行计算,最后的结果加上bias
@@ -76,7 +86,7 @@ class SecConv2d(Layer):
         if self.bias is None:
             pass
         else:
-            bias = ShareFloat(self.bias, p, tcp)
+            bias = ShareFloat(self.bias, p, tcp, device=self.device)
             output = output + bias
 
         output.value = (output.value.transpose(1, 2)).reshape(self.out_shape)
@@ -84,49 +94,57 @@ class SecConv2d(Layer):
         return output
 
     def __call__(self, x: ShareFloat):
-        return self.forward(x)
+        x_ = ShareFloat(x.value, x.p, x.tcp, device=x.device)
+        return self.forward(x_)
 
 
 class SecLinear(Layer):
     def __init__(self, weight, bias, name="SecLinear"):
-        super().__init__(name)
-        self.weight = ShareFloat(value=weight.T, p=0, tcp=None)
-        self.bias = ShareFloat(value=bias, p=0, tcp=None)
+        super(SecLinear, self).__init__(name=name)
+        self.weight = ShareFloat(value=weight.T, p=0, tcp=None, device=None)
+        self.bias = ShareFloat(value=bias, p=0, tcp=None, device=None)
 
     def forward(self, x: ShareFloat):
         self.weight.p = self.bias.p = x.p
         self.weight.tcp = self.bias.tcp = x.p
+        self.weight.device = self.bias.device = x.device
         x.value = x.value.reshape(x.value.shape[0], -1)
         z = (x @ self.weight) + self.bias
         return z
 
-    def __call__(self, *args, **kwargs):
-        return self.forward(*args, **kwargs)
+    def __call__(self, x: ShareFloat):
+        x_ = ShareFloat(x.value, x.p, x.tcp, x.device)
+        return x_
 
 
 class SecReLu(Layer):
     def __init__(self, name="SecReLu"):
-        super().__init__(name)
+        super(SecReLu, self).__init__(name=name)
 
     def forward(self, x: ShareFloat):
-        temp = ShareFloat(value=(torch.zeros(x.value.shape, dtype=torch.int64)), p=x.p, tcp=x.tcp)
+        temp = ShareFloat(value=(torch.zeros(x.value.shape, dtype=torch.long)), p=x.p, tcp=x.tcp, device=x.device)
         z = (x > temp) * scaled
         z = z * x
         return z
 
     def __call__(self, x: ShareFloat):
-        return self.forward(x)
+        x_ = ShareFloat(x.value, x.p, x.tcp, x.device)
+        return self.forward(x_)
 
 
 class SecAvgPool2D(Layer):
     def __init__(self, kernel_shape, stride, padding=(0, 0), name="SecAvgPool2D"):
-        super().__init__(name)
-        if type(kernel_shape) is tuple:
+        super(SecAvgPool2D, self).__init__(name=name)
+        if type(kernel_shape) in (tuple, list):
             self.kernel_shape = kernel_shape[0]
         else:
             self.kernel_shape = kernel_shape
+        if type(stride) in (tuple, list):
+            stride = stride[0]
+        else:
+            stride = stride
         self.stride = stride
-        if type(padding) is tuple:
+        if type(padding) in (tuple, list):
             padding = padding[0]
         else:
             padding = padding
@@ -144,24 +162,33 @@ class SecAvgPool2D(Layer):
         x.value = F.pad(x.value, (padding, padding, padding, padding), mode="constant", value=0)
 
         z = F.avg_pool2d(x.value, self.kernel_shape, self.stride)
-        res = ShareFloat(value=z.transpose(0, 1), p=x.p, tcp=x.tcp)
+        res = ShareFloat(value=z.transpose(0, 1), p=x.p, tcp=x.tcp, device=x.device)
 
         return res
 
     def __call__(self, x: ShareFloat):
-        return self.forward(x)
+        x_ = ShareFloat(x.value, x.p, x.tcp, x.device)
+        return self.forward(x_)
 
 
 class SecMaxPool2D(Layer):
-    def __init__(self, kernel_shape, stride, padding=(0, 0), name="SecMaxPool2D"):
-        super().__init__(name)
-        self.kernel_shape = kernel_shape
+    def __init__(self, kernel_shape, stride, padding=(0, 0), device="cpu", name="SecMaxPool2D"):
+        super(SecMaxPool2D, self).__init__(name=name)
+        if type(kernel_shape) in (tuple, list):
+            self.kernel_shape = kernel_shape[0]
+        else:
+            self.kernel_shape = kernel_shape
+        if type(stride) in (tuple, list):
+            stride = stride[0]
+        else:
+            stride = stride
         self.stride = stride
-        if type(padding) is tuple:
+        if type(padding) in (tuple, list):
             padding = padding[0]
         else:
             padding = padding
         self.padding = padding
+        self.device = device
 
     def get_out_shape(self, x: ShareFloat):
         n, img_c, img_h, img_w = x.value.shape
@@ -170,15 +197,15 @@ class SecMaxPool2D(Layer):
 
         return (n, img_c, out_h, out_w)
 
-    def sec_max(self, z, p, tcp):
+    def sec_max(self, z, p, tcp, device):
         def max_(z):
             if z.shape[1] == 1:
                 return z
             if z.shape[1] % 2 == 1:
                 z_ = z[:, -1:, :]
                 z = torch.cat((z, z_), dim=1)
-            z0 = ShareFloat(value=z[:, 0::2, :], p=p, tcp=tcp)
-            z1 = ShareFloat(value=z[:, 1::2, :], p=p, tcp=tcp)
+            z0 = ShareFloat(value=z[:, 0::2, :], p=p, tcp=tcp, device=device)
+            z1 = ShareFloat(value=z[:, 1::2, :], p=p, tcp=tcp, device=device)
 
             b0 = (z0 >= z1)
             b1 = (z1 > z0)
@@ -195,7 +222,7 @@ class SecMaxPool2D(Layer):
             return z
         else:
             z = max_(z)
-        return self.sec_max(z, p, tcp)
+        return self.sec_max(z, p, tcp, device)
 
     def forward(self, x: ShareFloat):
         N, C, W, H = x.value.shape
@@ -207,24 +234,25 @@ class SecMaxPool2D(Layer):
         padding = self.padding
         x.value = F.pad(x.value, (padding, padding, padding, padding), mode="constant", value=0)
 
-        x.value = ssf.img2col(x.value, k_size, stride)
+        x.value = ssf.img2col(x.value, k_size, stride, device=self.device)
         xs = []
         for i in range(0, C):
-            xs.append(self.sec_max(x.value[:, i * k_size * k_size:(i + 1) * k_size * k_size, :], x.p, x.tcp))
+            xs.append(self.sec_max(x.value[:, i * k_size * k_size:(i + 1) * k_size * k_size, :], x.p, x.tcp, x.device))
             # temp = temp.reshape(out_shape[0], out_shape[2], out_shape[3])
             # xs.append(temp)
 
-        xs = torch.cat(xs, dim=1).reshape(out_shape)
+        xs = torch.cat(xs, dim=1).reshape(out_shape).to(self.device)
 
-        return ShareFloat(xs, x.p, x.tcp)
+        return ShareFloat(xs, x.p, x.tcp, x.device)
 
     def __call__(self, x: ShareFloat):
-        return self.forward(x)
+        x_ = ShareFloat(x.value, x.p, x.tcp, x.device)
+        return self.forward(x_)
 
 
 class Flatten(Layer):
     def __init__(self, name="Flatten"):
-        super().__init__(name)
+        super(Flatten, self).__init__(name=name)
 
     def output_shape(self, x):
         n, c, w, h = x.value.shape
@@ -234,3 +262,154 @@ class Flatten(Layer):
         out_shape = self.output_shape(x)
         x.value = x.value.reshape(out_shape)
         return x
+
+
+class SecMatMul(Layer):
+    def __init__(self, name="SecMatMul"):
+        super(SecMatMul, self).__init__(name=name)
+
+    def __call__(self, x: ShareFloat, y: ShareFloat):
+        return x @ y
+
+
+class SecADD(Layer):
+    def __init__(self, name="SecADD"):
+        super(SecADD, self).__init__(name=name)
+
+    def __call__(self, x: ShareFloat, y: ShareFloat):
+        z = x + y
+        return z
+
+
+class SecTranspose(Layer):
+    def __init__(self, weight, name="SecTranspose"):
+        super(SecTranspose, self).__init__(name=name)
+        self.weight = weight
+
+    def __call__(self):
+        y = self.weight.value.T
+        return ShareFloat(y, self.weight.p, self.weight.tcp, self.weight.device)
+
+
+class SecReshape(Layer):
+    def __init__(self, name="SecReshape"):
+        super(SecReshape, self).__init__(name=name)
+
+    def __call__(self, x: ShareFloat, shape):
+        y = x.value.reshape(tuple(shape.value.tolist()))
+        return ShareFloat(y, x.p, x.tcp, x.device)
+
+
+class SecGemm(Layer):
+    def __init__(self, weight, bias, device, name="SecGemm"):
+        super(SecGemm, self).__init__(name=name)
+        self.device = device
+        self.weight = ShareFloat(value=weight.T, p=0, tcp=None, device=device)
+        self.bias = ShareFloat(value=bias, p=0, tcp=None, device=device)
+
+    def forward(self, x):
+        self.weight.p = self.bias.p = x.p
+        self.weight.tcp = self.bias.tcp = x.tcp
+        z = (x @ self.weight) + self.bias
+        return z
+
+    def __call__(self, x: ShareFloat):
+        return self.forward(x)
+
+
+class SecPad(Layer):
+    def __init__(self, mode, value=0, name="SecPad"):
+        super(SecPad, self).__init__(name=name)
+        self.mode = mode
+        self.value = value
+
+    def forward(self, x, pad):
+        y = torch.nn.functional.pad(x.value, pad.value.tolist(), self.mode, self.value)
+        return ShareFloat(y, x.p, x.tcp, x.device)
+
+    def __call__(self, x: ShareFloat, pad):
+        return self.forward(x, pad)
+
+
+class SecUnsqueeze(Layer):
+    def __init__(self, dim, name="SecUnsqueeze"):
+        super(SecUnsqueeze, self).__init__(name=name)
+        self.dim = dim
+
+    def forward(self, x):
+        y = torch.unsqueeze(x.value, self.dim.value.item())
+        return ShareFloat(y, x.p, x.tcp, x.device)
+
+    def __call__(self, x: ShareFloat):
+        return self.forward(x)
+
+
+class SecConcat(Layer):
+    def __init__(self, axis, name="SecConcat"):
+        super(SecConcat, self).__init__(name=name)
+        self.axis = axis
+
+    def forward(self, x, y):
+        z = torch.cat((x.value, y.value), dim=self.axis)
+        return ShareFloat(z, x.p, x.tcp, x.device)
+
+    def __call__(self, x: ShareFloat, y: ShareFloat):
+        return self.forward(x, y)
+
+
+class SecCopy(Layer):
+    def __init__(self, name="SecCopy"):
+        super().__init__(name=name)
+
+    def forward(self, x):
+        y = ShareFloat(x.value, x.p, x.tcp)
+        return y
+
+    def __call__(self, x: ShareFloat):
+        return self.forward(x)
+
+
+class SecBN2d(Layer):
+    def __init__(self, gamma, beta, running_mean, running_var, epsilon, name="SecBN2d"):
+        super().__init__(name=name)
+        self.gamma = gamma
+        self.beta = beta
+        self.running_mean = running_mean
+        self.running_var = running_var
+        self.epsilon = epsilon
+
+    def forward(self, x: ShareFloat):  # # Y = (X - running_mean) / sqrt(running_var + eps) * gamma + beta # #
+
+        self.running_mean.value = self.running_mean.value.unsqueeze(1)
+        self.running_mean.value = self.running_mean.value.unsqueeze(2)
+        self.running_mean.value = self.running_mean.value.repeat(1, x.value.shape[2], x.value.shape[3])
+
+        self.running_var = self.running_var.unsqueeze(1)
+        self.running_var = self.running_var.unsqueeze(2)
+
+        ssf.debug(x)
+
+        x = x - self.running_mean
+
+        divide = torch.floor(self.running_var + self.epsilon).long()
+        print(divide)
+        print(x.value)
+        # x.value = torch.floor(x.value / (self.running_var + self.epsilon))
+        x.value = torch.floor(torch.true_divide(x.value, divide).long())
+        print(x.value)
+        ssf.debug(x)
+
+        self.gamma.value = self.gamma.value.unsqueeze(1)
+        self.gamma.value = self.gamma.value.unsqueeze(2)
+
+        self.beta.value = self.beta.value.unsqueeze(1)
+        self.beta.value = self.beta.value.unsqueeze(2)
+
+        x = x * self.gamma
+        x = x + self.beta
+
+        return x
+
+    def __call__(self, x: ShareFloat):
+        x_ = ShareFloat(x.value, x.p, x.tcp, x.device)
+        return self.forward(x_)
--- ModelAndLayers/model/modeloflayers.py
+++ ModelAndLayers/model/modeloflayers.py
@@ -4,82 +4,96 @@
 # @File : model.py
 """
 import time
-
+from ProtocolOnRing import secret_sharing_fixpoint as ssf
 
 class ModelOfLayers(object):
-    def __init__(self, ):
+    def __init__(self, input_names=None, output_name=None):
         self.layers = []
         self.input = None
+        self.input_names = input_names
+        self.output_name = output_name
+        self.output = {}
 
     def add(self, layer):
         self.layers.append(layer)
 
     def set_input(self, X):
         self.input = X
+        self.output["input"] = X
 
     def predict(self):
         for i, layer in enumerate(self.layers):
 
             if layer.name == "Conv2D":
-                # print("**************************Conv2D开始啦**************************")
-                # start_time = time.time()
-                self.input = layer(self.input)
-                # end_time = time.time()
-                # print("卷积层计算时间", end_time - start_time)
-                # print("**************************Conv2D结束啦**************************")
-                # print()
+                # print("==================分割线====================")
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
+                # print("Conv2D")
+                # ssf.debug(output)
 
             elif layer.name == "SecReLu":
-                # print("**************************SecReLu层开始啦**************************")
-                # start_time = time.time()
-                self.input = layer(self.input)
-                # ssf.debug(self.input)
-                # end_time = time.time()
-                # print("Relu层计算时间", end_time - start_time)
-                # print("**************************SecReLu层结束啦**************************")
-                # print()
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
+                # print("SecReLu")
+                # ssf.debug(output)
 
             elif layer.name == "SecLinear":
-                # print("**************************线性层开始啦**************************")
-                # start_time = time.time()
-                self.input = layer(self.input)
-                # end_time = time.time()
-                # print("Linear层计算时间", end_time - start_time)
-                # print("**************************线性层结束啦**************************")
-                # print()
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
 
             elif layer.name == "SecAvgPool2D":
-                # print("**************************SecAvgPool2D开始啦**************************")
-                # start_time = time.time()
-                self.input = layer(self.input)
-                # end_time = time.time()
-                # print("SecAvgPool2D计算时间", end_time - start_time)
-                # print("**************************SecAvgPool2D结束啦**************************")
-                # print()
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
 
             elif layer.name == "SecMaxPool2D":
-                # print("**************************SecMaxPool2D开始啦**************************")
-                # start_time = time.time()
-                self.input = layer(self.input)
-                # end_time = time.time()
-                # print("SecMaxPool2D计算时间", end_time - start_time)
-                # print("**************************SecMaxPool2D结束啦**************************")
-                # print()
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
+                # print("SecMaxPool2D")
+                # ssf.debug(output)
 
             elif layer.name == "BatchNormalization":
-                # print("**************************BatchNormalization开始啦**************************")
-                # start_time = time.time()
-                self.input = layer(self.input)
-                # end_time = time.time()
-                # print("计算时间", end_time - start_time)
-                # print("**************************BatchNormalization结束啦**************************")
-                # print()
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
 
             elif layer.name == "Flatten":
-                # print("**************************flatten开始啦**************************")
-                # start_time = time.time()
-                self.input = layer(self.input)
-                # end_time = time.time()
-                # print("计算时间", end_time - start_time)
-                # print("**************************flatten结束啦**************************")
-                # print()
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecADD":
+                output = layer(self.output[layer.input_name[0]], self.output[layer.input_name[1]])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecMatMul":
+                output = layer(self.output[layer.input_name[0]], self.output[layer.input_name[1]])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecTranspose":
+                output = layer()
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecReshape":
+                output = layer(self.output[layer.input_name[0]], self.output[layer.input_name[1]])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecGemm":
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecPad":
+                output = layer(self.output[layer.input_name[0]], self.output[layer.input_name[1]])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecUnsqueeze":
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecConcat":
+                output = layer(self.output[layer.input_name[0]], self.output[layer.input_name[1]])
+                self.output[layer.output_name] = output
+
+            elif layer.name == "SecBN2d":
+                output = layer(self.output[layer.input_name])
+                self.output[layer.output_name] = output
+                # print("SecBN2d")
+                # ssf.debug(output)
+
