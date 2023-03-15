0bfaf3dc5099c28193fa40f93f32939842a0b5fb
ruetrash <bigtimerr@163.com>
2023-03-15 11:52:38 +0800

    MSB三元组文件更新

--- MSB/msb_triples_vector.py
+++ MSB/msb_triples_vector.py
@@ -5,14 +5,18 @@
 """
 import time
 import numpy as np
-from multiprocessing import shared_memory
 import ProtocolOnRing.param as param
 import torch
 
+"""
+    使用到MSB之前必须打开下面的注释运行程序，只需要打开一层注释部分即可
+"""
+
+
 Ring = param.Ring
-data_path = "./MSB/data_vector/"
-k = 100
-n = 20000
+data_path = "./MSB/MSB_triples_data/"
+k = 35
+n = 20000000
 
 # ################################################################################
 # # def share_bool(t):
@@ -57,107 +61,16 @@ c_1 = torch.load(data_path + "c_1.pth")
 
 
 def get_triples_msb(p, ptr, n, l):
-    '''
-
+    """
     :param p: 客户端还是服务器
     :param ptr: 从第几个数据开始取
     :param n: 取几维的数据
     :param l: 每一维度去多少数据
     :return: a,b,c三元组
-    '''
-    # print("triples: a: %d, b: %d ,c :%d" % (a[ptr].item(), b[ptr].item(), c[ptr].item()))
+    """
 
     if p == 0:
-        # print("triples: a0: %d, b0: %d ,c0 :%d" % (a_0[ptr].item(), b_0[ptr].item(), c_0[ptr].item()))
         return a_0[:n, ptr:ptr + l], b_0[:n, ptr:ptr + l], c_0[:n, ptr:ptr + l]
     else:
-        # print("triples: a1: %d, b1: %d ,c1 :%d" % (a_1[ptr].item(), b_1[ptr].item(), c_1[ptr].item()))
         return a_1[:n, ptr:ptr + l], b_1[:n, ptr:ptr + l], c_1[:n, ptr:ptr + l]
 
-
-#
-# Ring = param.Ring_2
-# k = 4096
-# global buffer
-#
-#
-# def share_value(t):
-#     t_0 = np.random.randint(0, 2, size=k, dtype=bool)
-#     t_1 = t ^ t_0
-#     return t_0, t_1
-#
-#
-# def generate_triples():
-#     a = np.random.randint(0, 2, size=k, dtype=bool)
-#     b = np.random.randint(0, 2, size=k, dtype=bool)
-#     c = a ^ b
-#     return a, b, c
-#
-#
-# def close_shm(shm):
-#     shm.close()
-#     shm.unlink()
-#
-#
-# def get_triples(p, ptr):
-#     if p == 0:
-#         shm_a_0 = shared_memory.SharedMemory(name="a_0", size=k)
-#         a_0 = shm_a_0.buf[ptr]
-#         shm_b_0 = shared_memory.SharedMemory(name="b_0", size=k)
-#         b_0 = shm_b_0.buf[ptr]
-#         shm_c_0 = shared_memory.SharedMemory(name="c_0", size=k)
-#         c_0 = shm_c_0.buf[ptr]
-#         close_shm(shm_a_0)
-#         close_shm(shm_b_0)
-#         close_shm(shm_c_0)
-#         return a_0, b_0, c_0
-#     else:
-#         shm_a_1 = shared_memory.SharedMemory(name="a_1", size=k)
-#         a_1 = shm_a_1.buf[ptr]
-#         shm_b_1 = shared_memory.SharedMemory(name="b_1", size=k)
-#         b_1 = shm_b_1.buf[ptr]
-#         shm_c_1 = shared_memory.SharedMemory(name="c_1", size=k)
-#         c_1 = shm_c_1.buf[ptr]
-#         close_shm(shm_a_1)
-#         close_shm(shm_b_1)
-#         close_shm(shm_c_1)
-#         return a_1, b_1, c_1
-#
-#
-# def showData(shm):
-#     print(shm.buf.tolist())
-#
-#
-# if __name__ == '__main__':
-#     shm_a = shared_memory.SharedMemory(create=True, name="a", size=k)
-#     shm_b = shared_memory.SharedMemory(create=True, name="b", size=k)
-#     shm_c = shared_memory.SharedMemory(create=True, name="c", size=k)
-#     shm_a_0 = shared_memory.SharedMemory(create=True, name="a_0", size=k)
-#     shm_a_1 = shared_memory.SharedMemory(create=True, name="a_1", size=k)
-#     shm_b_0 = shared_memory.SharedMemory(create=True, name="b_0", size=k)
-#     shm_b_1 = shared_memory.SharedMemory(create=True, name="b_1", size=k)
-#     shm_c_0 = shared_memory.SharedMemory(create=True, name="c_0", size=k)
-#     shm_c_1 = shared_memory.SharedMemory(create=True, name="c_1", size=k)
-#
-#     # 每分钟刷新一次a,b,c 的值
-#     while 1:
-#         a, b, c = generate_triples()
-#
-#         a_0, a_1 = share_value(a)
-#         b_0, b_1 = share_value(b)
-#         c_0, c_1 = share_value(c)
-#
-#         shm_a.buf[:] = bytearray(a)
-#         shm_b.buf[:] = bytearray(b)
-#         shm_c.buf[:] = bytearray(c)
-#         shm_a_0.buf[:] = bytearray(a_0)
-#         shm_a_1.buf[:] = bytearray(a_1)
-#         shm_b_0.buf[:] = bytearray(b_0)
-#         shm_b_1.buf[:] = bytearray(b_1)
-#         shm_c_0.buf[:] = bytearray(c_0)
-#         shm_c_1.buf[:] = bytearray(c_1)
-#
-#         showData(shm_a)
-#         showData(shm_a_0)
-#         showData(shm_a_1)
-#         time.sleep(60)
